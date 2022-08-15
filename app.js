ede78b8e36830a7c0c57b5ed04e1cf7520dbf7e1
FoxCrush <63466872+FoxCrush@users.noreply.github.com>
2022-08-15 18:55:57 +0300

    crud added

--- app.js
+++ app.js
@@ -1,47 +1,86 @@
-// const os = require("os");
-// const fs = require("fs");
+const Joi = require("joi");
+const express = require("express");
+const app = express();
 
-// const Logger = require("./logger");
-// const logger = new Logger();
+app.use(express.json());
 
-// //regiter a listner
-// logger.on("messageLogged", (arg) => {
-//   console.log("Listner called", arg);
-// });
+const animalList = [
+  { id: 1, name: "cat" },
+  { id: 2, name: "dog" },
+];
 
-// const files = fs.readdirSync("./");
-// console.log(files);
-
-// fs.readdir("./", function (err, files) {
-//   if (err) console.log("ERROR", err);
-//   else console.log("ASYNC RESULT", files);
-// });
-
-// const totalMemory = os.totalmem;
-// const freeMemory = os.freemem;
-// console.log(`Total Memory: ${totalMemory}`);
-// console.log(`Free Memory: ${freeMemory}`);
-
-// // //one more test event listner
-// // emitter.on("testLogging", ({ data }) => {
-// //   console.log(data);
-// // });
-// // //one more test event evocing
-// // emitter.emit("testLogging", { data: "test logging message" });
-
-// logger.log("sent message");
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("RESPONSE MSG");
-    res.end();
+app.get("/", (req, res) => {
+  res.send("This is home page");
+});
+//get all animals
+app.get("/api/list", (req, res) => {
+  res.send(animalList);
+});
+//get animal by id
+app.get("/api/list/:id", (req, res) => {
+  const animal = animalList.find((item) => item.id === parseInt(req.params.id));
+  if (!animal) {
+    res.status(404).send("NOT FOUND MESSAGE");
+    return;
   }
-
-  if (req.url === "/data") {
-    res.write("SENDING DATA");
-    res.end();
-  }
+  res.send(animal);
 });
 
-server.listen(3000);
+//post new item
+app.post("/api/list", (req, res) => {
+  const { error } = validateAnimal(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  const animal = {
+    id: animalList.length + 1,
+    name: req.body.name,
+  };
+  animalList.push(animal);
+  res.send(animal);
+});
+
+// update item
+app.put("/api/list/:id", (req, res) => {
+  //check if animal exists
+  const animal = animalList.find((item) => item.id === parseInt(req.params.id));
+  if (!animal) {
+    res.status(404).send("NOT FOUND MESSAGE");
+    return;
+  }
+
+  //validate
+  const { error } = validateAnimal(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  animal.name = req.body.name;
+  res.send(animal);
+});
+//from validation we need only error
+function validateAnimal(animal) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+  });
+  return schema.validate(animal);
+}
+
+app.delete("/api/list/:id", (req, res) => {
+  const animal = animalList.find((item) => item.id === parseInt(req.params.id));
+  if (!animal) {
+    res.status(404).send("NOT FOUND MESSAGE");
+    return;
+  }
+
+  const index = animalList.indexOf(animal);
+  animalList.splice(index, 1);
+  res.send(animal);
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
